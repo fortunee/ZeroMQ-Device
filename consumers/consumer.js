e45e3f95170e352f8a1c945caf424d9efb353a48
Fortune Ekeruo <legalmody@gmail.com>
2018-02-02 17:18:22 +0100

    Fetch data via mockapi and send it to connected consumers

--- consumers/consumer.js
+++ consumers/consumer.js
@@ -3,7 +3,7 @@ import zmq from 'zeromq';
 const socket = zmq.socket('pull');
 
 socket.on('message', (message) => {
-    console.log('Received ', message.toString('utf8'));
+    console.log('Received ', JSON.parse(message));
 });
 
 socket.connect('tcp://127.0.0.1:5600');
--- producers/producer.js
+++ producers/producer.js
@@ -1,11 +1,26 @@
 import zmq from 'zeromq';
+import axios from 'axios';
+import { start } from 'repl';
 
 const socket = zmq.socket('push');
+const URL = 'http://5a746a4e61c2a40012894a84.mockapi.io/api/orders';
 let counter = 0;
 
-socket.connect('tcp://127.0.0.1:5678');
-
 setInterval(() => {
-    socket.send(counter)
-    console.log('Sent', counter++);
+    axios.get(URL).then((orders) => {
+        if (counter < 100) {
+            socket.send(JSON.stringify({name: orders.data[counter] }))
+            console.log('New data sent...', counter++);
+        } else {
+            console.log('Job done')
+            process.exit(0);
+        }
+    }).catch(err => {
+        console.error(err)
+        process.exit(0);
+    });
 }, 1000)
+
+console.log('Getting ready...');
+
+socket.connect('tcp://127.0.0.1:5678');
